fb073abbca900461a266728dd86b5fce99ff163b
Ekaterina <ekaterina.matvyeyeva@gmail.com>
2020-02-13 20:57:05 +0000

    Fix webhook

--- server/expressCallbacks/initUserSession.js
+++ server/expressCallbacks/initUserSession.js
@@ -1,4 +1,3 @@
-import ngrok from 'ngrok';
 import bot, { BotMessage } from '../botSetUp';
 import UserRepository from '../UserRepository';
 import samples from '../viberDataSamples';
@@ -10,12 +9,6 @@ const userRepository = new UserRepository(bot);
 
 const initUserSession = async (req, res) => {
   res.send(`Bot session is started`);
-  const exposeUrl = await  ngrok.connect(port);
-
-  await bot.setWebhook(`${exposeUrl}/viber/webhook`).catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
 
   const userProfile = await userRepository.saveLastMember();
 
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import ngrok from 'ngrok';
 import express from 'express';
 import bot from './botSetUp';
 import callbacks from "./expressCallbacks";
@@ -14,12 +15,16 @@ if (!process.env.BOT_ACCOUNT_TOKEN) {
 app.use("/viber/webhook", bot.middleware());
 
 app.listen(port, async () => {
+  const exposeUrl = await  ngrok.connect(port);
+
+  await bot.setWebhook(`${exposeUrl}/viber/webhook`).catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
   console.log(`Application running on port: ${port}`);
 });
 
 app.get('/init_user_session', callbacks.initUserSession);
-
-
 
 
 export default app;
